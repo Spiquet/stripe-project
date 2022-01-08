42c13ae9fe39873a7b39988c9526567944a5db48
Simon Piquet <piquet.simon@yahoo.fr>
2022-01-08 15:14:07 +0100

    feat(server): store session in cookie

--- server/src/resolver.ts
+++ server/src/resolver.ts
@@ -4,10 +4,14 @@ import { User } from "./entity/User"
 
 export const resolvers = {
     Query: {
-        hello: () => "Hi"
+        me: (_: any, __: any, { req }: any) => {
+            if (!req.session.userId) {
+                return null
+            }
+            return User.findOne(req.seesion.userId);
+        }
     },
-
-    Mutation: {
+     Mutation: {
         register: async (_: any, { email, password }: any) => {
             const hashedPassword = await bcrypt.hash(password, 10);
             await User.create({
@@ -17,14 +21,21 @@ export const resolvers = {
 
             return true;
         },
-        login: async (_: any, { email, password }: any) => {
+        login: async (_: any, { email, password }: any, { req }: any) => {
             const user = await User.findOne({ where: { email } });
             if (!user) {
                 return null;
             }
+
             const valid = await bcrypt.compare(password, user.password)
             if (!valid) {
+                return null;
             }
+
+            // Who why know that the user is who he is ?
+            // We store a cookie on him and store the user Id on our server
+            req.session.userId = user.id;
+            
             return user;
         }
     }
