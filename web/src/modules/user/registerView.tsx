835a95c7a81fd0b3fcf8a4dcaea26c954008b20d
Simon Piquet <piquet.simon@yahoo.fr>
2022-01-09 17:57:49 +0100

    feat(web-client):create register form

--- web/src/modules/user/registerView.tsx
+++ web/src/modules/user/registerView.tsx
@@ -3,8 +3,45 @@ import React from "react";
 
 
 export class RegisterView extends React.PureComponent {
-render() {
-    return <div>register view</div>
-}
-    
+
+    state = {
+        email: "",
+        password: ""
+    };
+
+    handleChange = (e: any) => {
+        const { name, value } = e.target;
+        this.setState({
+            [name]: value
+        })
+    }
+
+    render() {
+        const { password, email } = this.state
+        return (
+            <form style={{ display: "flex", flexDirection: "column", alignItems:"center" }}>
+                <div>
+                    <input
+                        name="email"
+                        placeholder="email"
+                        value={email}
+                        onChange={this.handleChange}
+                    />
+                </div>
+                <div>
+                    <input
+                        type="password"
+                        name="password"
+                        placeholder="paswword"
+                        value={password}
+                        onChange={this.handleChange}
+                    />
+                </div>
+                <div>
+                    <button onClick={() => console.log("hi")}>Register</button>
+                </div>
+            </form>
+        );
+    }
+
 }
