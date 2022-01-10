8a9a6d26688a074e95d1336167840763eb3cd0a8
Simon Piquet <piquet.simon@yahoo.fr>
2022-01-10 10:35:39 +0100

    generate types

---
+++ web/src/generated/graphql.tsx
@@ -0,0 +1,87 @@
+import { gql } from '@apollo/client';
+import * as Apollo from '@apollo/client';
+export type Maybe<T> = T | null;
+export type InputMaybe<T> = Maybe<T>;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+const defaultOptions =  {}
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
+};
+
+export type Mutation = {
+  __typename?: 'Mutation';
+  login?: Maybe<User>;
+  register: Scalars['Boolean'];
+};
+
+
+export type MutationLoginArgs = {
+  email: Scalars['String'];
+  password: Scalars['String'];
+};
+
+
+export type MutationRegisterArgs = {
+  email: Scalars['String'];
+  password: Scalars['String'];
+};
+
+export type Query = {
+  __typename?: 'Query';
+  me?: Maybe<User>;
+};
+
+export type User = {
+  __typename?: 'User';
+  email: Scalars['String'];
+  id: Scalars['ID'];
+};
+
+export type RegisterMutationMutationVariables = Exact<{
+  email: Scalars['String'];
+  password: Scalars['String'];
+}>;
+
+
+export type RegisterMutationMutation = { __typename?: 'Mutation', register: boolean };
+
+
+export const RegisterMutationDocument = gql`
+    mutation RegisterMutation($email: String!, $password: String!) {
+  register(email: $email, password: $password)
+}
+    `;
+export type RegisterMutationMutationFn = Apollo.MutationFunction<RegisterMutationMutation, RegisterMutationMutationVariables>;
+
+/**
+ * __useRegisterMutationMutation__
+ *
+ * To run a mutation, you first call `useRegisterMutationMutation` within a React component and pass it any options that fit your needs.
+ * When your component renders, `useRegisterMutationMutation` returns a tuple that includes:
+ * - A mutate function that you can call at any time to execute the mutation
+ * - An object with fields that represent the current status of the mutation's execution
+ *
+ * @param baseOptions options that will be passed into the mutation, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options-2;
+ *
+ * @example
+ * const [registerMutationMutation, { data, loading, error }] = useRegisterMutationMutation({
+ *   variables: {
+ *      email: // value for 'email'
+ *      password: // value for 'password'
+ *   },
+ * });
+ */
+export function useRegisterMutationMutation(baseOptions?: Apollo.MutationHookOptions<RegisterMutationMutation, RegisterMutationMutationVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useMutation<RegisterMutationMutation, RegisterMutationMutationVariables>(RegisterMutationDocument, options);
+      }
+export type RegisterMutationMutationHookResult = ReturnType<typeof useRegisterMutationMutation>;
+export type RegisterMutationMutationResult = Apollo.MutationResult<RegisterMutationMutation>;
+export type RegisterMutationMutationOptions = Apollo.BaseMutationOptions<RegisterMutationMutation, RegisterMutationMutationVariables>;
--- web/src/modules/user/registerView.tsx
+++ web/src/modules/user/registerView.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { Mutation } from "react-apollo";
+import { gql } from "apollo-boost";
+import { RegisterMutationMutation, RegisterMutationMutationVariables } from "../../generated/graphql";
 
 
 
+const registerMutation = gql`
+mutation RegisterMutation($email: String!, $password: String!) {
+    register(email: $email, password: $password)
+  }
+  `;
 export class RegisterView extends React.PureComponent {
 
     state = {
@@ -19,28 +27,33 @@ export class RegisterView extends React.PureComponent {
     render() {
         const { password, email } = this.state
         return (
-            <form style={{ display: "flex", flexDirection: "column", alignItems:"center" }}>
-                <div>
-                    <input
-                        name="email"
-                        placeholder="email"
-                        value={email}
-                        onChange={this.handleChange}
-                    />
-                </div>
-                <div>
-                    <input
-                        type="password"
-                        name="password"
-                        placeholder="paswword"
-                        value={password}
-                        onChange={this.handleChange}
-                    />
-                </div>
-                <div>
-                    <button onClick={() => console.log("hi")}>Register</button>
-                </div>
-            </form>
+            <Mutation<RegisterMutationMutation, RegisterMutationMutationVariables> mutation={registerMutation}>
+                {() => (
+
+                    <form style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+                        <div>
+                            <input
+                                name="email"
+                                placeholder="email"
+                                value={email}
+                                onChange={this.handleChange}
+                            />
+                        </div>
+                        <div>
+                            <input
+                                type="password"
+                                name="password"
+                                placeholder="paswword"
+                                value={password}
+                                onChange={this.handleChange}
+                            />
+                        </div>
+                        <div>
+                            <button onClick={() => console.log("hi")}>Register</button>
+                        </div>
+                    </form>
+                )}
+            </Mutation>
         );
     }
 
