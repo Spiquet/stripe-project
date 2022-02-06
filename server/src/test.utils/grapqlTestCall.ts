ad6331c58b6e63b2563e39b765b8b62371289d86
Simon Piquet <piquet.simon@yahoo.fr>
2022-02-06 16:12:21 +0100

    feat(test-docker-image):#1

--- server/src/test.utils/grapqlTestCall.ts
+++ server/src/test.utils/grapqlTestCall.ts
@@ -4,14 +4,7 @@ import { resolvers } from "../resolvers";
 import { graphql } from "graphql";
 
 
-// interface Options {
-//   source: string;
-//   variableValues?: any;
-//   userId?: number | string;
-// }
-
 const schema = makeExecutableSchema({ typeDefs, resolvers });
-
 
 
 export const gCall = async (source?: any, variableValues?: any, userId?: number | string) => {
